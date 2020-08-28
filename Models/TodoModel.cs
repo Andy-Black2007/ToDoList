ad19418575eb3a7a1c8adb213ef7ff57c222dea6
YukiAma <Krupskiy25.12.00@gmail.com>
2020-08-28 19:46:00 +0300

    UI

---
+++ Models/TodoModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoApp.Models
+{
+    class TodoModel
+    {
+        private bool _isDone;
+        private string _text;
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public bool isDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+        public string text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+    }
+}
